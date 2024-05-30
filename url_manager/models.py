1d8ea4d6bf7971cdcd6f47e8f4ea9b7ccbe19fab
Stan Choi <sjc591@nyu.edu>
2024-05-30 11:44:06 -0400

    Working on Collection model

---
+++ url_manager/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Collection(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_public = models.BooleanField(default=False)
+    shared_with = models.ManyToManyField(User, related_name='shared_collections', blank=True)
+
+    def __str__(self):
+        return self.name
+
+class URL(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
